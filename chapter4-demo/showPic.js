60221494ce6f05ecd531cc69618c32d7707fe616
litianzhi <979638192@qq.com>
2018-03-08 22:48:32 +0800

    JavaScript图片库

--- chapter4-demo/showPic.js
+++ chapter4-demo/showPic.js
@@ -1,11 +1,16 @@
 function showPic(whichpic) {
     var source = whichpic.getAttribute("href");
+    var text = whichpic.getAttribute("title")
     var placeholder = document.getElementById("placeholder");
+    var description = document.getElementById("description");
     placeholder.setAttribute("src",source);
+    description.firstChild.nodeValue = text;
 }
 function countBodyChildren() {
     var body_element = document.getElementsByTagName("body")[0];
     var element_sum = body_element.childNodes.length;
     alert(element_sum);
+    alert(body_element.nodeType);
+    alert(description.childNodes[0].nodeValue);
 }
 window.onload = countBodyChildren;
